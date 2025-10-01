42b740b30f84513052f99f62f942cd947a7d45ff
lhalcomb <lhalcomb@drury.edu>
2025-10-01 16:32:57 -0500

    ch. 4 push

---
+++ ImplementingGPTModel/LLMArchitecture.py
@@ -0,0 +1,84 @@
+"""
+The beginnings of the LLM Architecture. 
+
+This file will be the source file to the GPT placeholder architecture of Dummy-GPTModel,
+to provide a big picture view of how everything will fit together so we can assemble 
+the full GPT model architecture. 
+"""
+import torch
+import tiktoken
+from DummyGPTModel import DummyGPTModel
+tokenizer = tiktoken.get_encoding("gpt2")
+
+GPT_CONFIG_124M = {
+    "vocab_size": 50257, # Vocabulary size
+    "context_length": 1024, # Context length
+    "emb_dim": 768, # Embedding dimension
+    "n_heads": 12, # Number of attention heads
+    "n_layers": 12, # Number of layers
+    "drop_rate": 0.1, # Dropout rate
+    "qkv_bias": False # Query-Key-Value bias
+}
+
+"""
+-- vocab_size refers to a vocabulary of 50,257 words, as used by the BPE tokenizer
+(see chapter 2).
+
+-- context_length denotes the maximum number of input tokens the model can
+handle via the positional embeddings (see chapter 2).
+
+-- emb_dim represents the embedding size, transforming each token into a 768-
+dimensional vector.
+
+-- n_heads indicates the count of attention heads in the multi-head attention
+mechanism (see chapter 3).
+
+-- n_layers specifies the number of transformer blocks in the model, which we
+will cover in the upcoming discussion.
+
+-- drop_rate indicates the intensity of the dropout mechanism (0.1 implies a 10%
+random drop out of hidden units) to prevent overfitting (see chapter 3).
+
+-- qkv_bias determines whether to include a bias vector in the Linear layers of
+the multi-head attention for query, key, and value computations. We will initially
+disable this, following the norms of modern LLMs, but we will revisit it in chapter 6 when we load pretrained GPT-2 weights from OpenAI into our model (see
+chapter 6)
+
+"""
+
+if __name__ == "__main__":
+
+    #4.1 Coding an LLM architecture
+    #Step1 - Implement dummy architecture
+
+
+
+    #These are the batches of token IDs for each text string 
+    batch = []
+    txt1 = "Every effort moves you"
+    txt2 = "Every day holds a"
+    batch.append(torch.tensor(tokenizer.encode(txt1)))
+    batch.append(torch.tensor(tokenizer.encode(txt2)))
+    batch = torch.stack(batch, dim=0)
+    print(batch) #prints = (tensor([[6109, 3626, 6100,  345], [6109, 1110, 6622,  257]]))
+
+
+    #next initialize the 124 million parameter DummyGPTModel
+    torch.manual_seed(123)
+    model = DummyGPTModel(GPT_CONFIG_124M)
+    logits = model(batch)
+    print("Output shape:", logits.shape)
+    print(logits)
+
+    #4.2 Normalizing activations with layer normalization
+    """
+    -- We will explore the layer normalization for normalizing activations. 
+    This improves the stability and efficiency of neural network training. 
+
+    -- We approach it with this idea:
+            layer normalization is just the adjustment of the activations (outputs) 
+            of a neural network layer to have a mean of 0 and a variance of 1, also
+            known as unit variance.
+    """
+
+    
